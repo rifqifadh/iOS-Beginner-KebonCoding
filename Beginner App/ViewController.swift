2a4e629fbe64976933220c3e552ab678b33219bf
rifqifadh <rifqi.fadhlillah@gmail.com>
2019-04-06 09:18:00 +0700

    first commit

---
+++ Beginner App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beginner App
+//
+//  Created by Bhayu Ravelli Arsyad on 06/04/19.
+//  Copyright © 2019 Rifqi Fadhlillah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Beginner AppTests/Beginner_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Beginner_AppTests.swift
+//  Beginner AppTests
+//
+//  Created by Bhayu Ravelli Arsyad on 06/04/19.
+//  Copyright © 2019 Rifqi Fadhlillah. All rights reserved.
+//
+
+import XCTest
+@testable import Beginner_App
+
+class Beginner_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
